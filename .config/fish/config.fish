d95ce0f681682c3540cff18662536f845a3a6f9b
1337Nerd <26494273+1337Nerd@users.noreply.github.com>
2018-01-24 15:41:05 -0500

    Update config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -9,9 +9,29 @@ function fish_prompt
     echo (set_color 087DC0)(pwd|sed "s=$HOME=~=")(set_color 847BE4)' » '(set_color normal)
 end
 
+function transfer
+    if test (count $argv) -eq 0
+        echo "No arguments specified. Usage:\necho transfer /tmp/test.md\ncat /tmp/test.md | transfer test.md"
+        return 1
+    end
+    set tmpfile ( mktemp -t transferXXX )
+    set file $argv[1]
+        set basefile (basename "$file" | sed -e 's/[^a-zA-Z0-9._-]/-/g')
+        if test -d $file
+            set zipfile ( mktemp -t transferXXX.zip )
+            zip -r -q - $file >> $zipfile
+            curl --progress-bar --upload-file "$zipfile" "https://transfer.sh/$basefile.zip" >> $tmpfile
+            rm -f $zipfile
+        else
+            curl --progress-bar --upload-file "$file" "https://transfer.sh/$basefile" >> $tmpfile
+        end
+    cat $tmpfile
+    rm -f $tmpfile
+end
+
 alias ...='cd /'
-alias update='pacaur -Syu --noconfirm; pacaur -Rns (pacaur -Qttdq) --noconfirm; pacaur -Sc --noconfirm; rm -rf /home/josh/.cache/*'
-alias moreupdate='pacaur -Syu --noconfirm; pacaur -Rns (pacaur -Qttdq) --noconfirm; pacaur -Scc; rm -rf /home/josh/.cache/*'
+alias update='pacaur -Syu --noconfirm; sudo pacman -Rns (sudo pacman -Qttdq) --noconfirm; pacaur -Sc --noconfirm; rm -rf /home/josh/.cache/*'
+alias moreupdate='pacaur -Syu --noconfirm; sudo pacman -Rns (sudo pacman -Qttdq) --noconfirm; pacaur -Scc; rm -rf /home/josh/.cache/*'
 alias pacman='sudo pacman'
 
 set -gx LESS_TERMCAP_mb \e"[01;31m" 
